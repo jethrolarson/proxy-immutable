009b7c0bea99325648938346b647c904c650996b
jethro larson <jethrolarson@sbgtv.com>
2016-05-17 13:29:25 -0700

    Added several methods and some tests

--- index.js
+++ index.js
@@ -1,24 +1,47 @@
 //<helpers>
 
+//:: a -> [a]
+const of = a => List([a])
+
 //:: [a] -> [a]
-const cloneArray = ar => ar.slice(0)
+const cloneArray = xs => xs.slice(0)
 
 //:: [a] -> a -> List a
-const appendTo = ar => v => List(ar.concat([v]))
+const appendTo = xs => v => List(xs.concat([v]))
 
 //:: [a] -> a -> List a
-const prependTo = ar => v => List([v].concat(ar))
+const prependTo = xs => v => List([v].concat(xs))
 
 //:: [a] -> (a -> b) -> [b]
-const mapTo = ar => f => List(ar.map(f))
+const mapTo = xs => f => List(xs.map(f))
+
+//:: [a] -> (a -> Boolean) -> [a]
+const filterTo = xs => pred => List(xs.filter(pred))
+
+//:: [a] -> (((b, a) -> Boolean), b) -> b
+const reduceTo = xs => (f, acc) => List(xs.reduce(f, acc))
+
+//:: [a] -> (((b, a) -> Boolean), b) -> b
+const reduceRightTo = xs => (f, acc) => List(xs.reduceRight(f, acc))
+
+//:: [a] -> [a] -> [a]
+const concatTo = xs => ys => List(xs.concat(ys))
+
+//:: [a] -> Number -> [a]
+//:: [a] -> (Number, Number) -> [a]
+//:: [a] -> (Number, Number, Number) -> [a]
+const sliceTo = xs => (...args) => List(xs.slice(...args))
 
 //:: [a] -> Int -> a -> [a]
-const updateTo = ar => (i, v) => {
-  if(i >= ar.length) throw new RangeError(`index '${i}' is not in array`)
-  const copy = cloneArray(ar)
+const updateTo = xs => (i, v) => {
+  if(i >= xs.length || i < 0) throw new RangeError(`index '${i}' is not in array`)
+  const copy = cloneArray(xs)
   copy[i] = v
   return List(copy)
 }
+
+const flatten = xs => List(xs.reduce((acc, it) => acc.concat(it), []))
+
 //</helpers>
 
 // List :: [a] -> List a
@@ -29,20 +52,45 @@ const List = (ar = []) => new Proxy(ar, {
         return updateTo(target)
       case 'map':
         return mapTo(target)
-      case 'conj':
+      case 'filter':
+        return filterTo(target)
       case 'append':
         return appendTo(target)
-      case 'cons':
       case 'prepend':
         return prependTo(target)
+      case 'concat':
+        return concatTo(target)
+      case 'reduce':
+        return reduceTo(target)
+      case 'reduceRight':
+        return reduceRightTo(target)
+      case 'slice':
+        return sliceTo(target)
+      case 'flatten':
+        return () => flatten(target)
+      //TODO
+      case 'insert':
+      case 'drop':
+      case 'take':
+      case 'takeLast':
+      case 'dropLast':
+      case 'flatMap':
+      case 'chain':
+      case 'zip':
+      case 'sort':
+      case 'sortBy':
+      case 'reverse':
+      case 'delete':
+        throw new TypeError(`Method \`${prop}\` has not yet been implemented`)
       // disallowed methods
       case 'push':
       case 'pop':
       case 'shift':
       case 'unshift':
       case 'splice':
+      case 'fill':
+      case 'copyWithin':
         throw new TypeError(`List does not allow method \`${prop}\``)
-
       default:
         return target[prop]
     }
@@ -51,6 +99,8 @@ const List = (ar = []) => new Proxy(ar, {
     throw new TypeError('List is not mutable. Use `update` method to return modified copy')
   }
 })
+
+Object.assign(List, {of, appendTo, prependTo, updateTo, mapTo, filterTo, reduceTo, concatTo})
 
 module.exports = {
   List
--- test.js
+++ test.js
@@ -1,10 +1,13 @@
 const test = require('tape-catch')
 const {List} = require('./index')
 
+const inc = a => a + 1
+
 test('basic array semantics', t => {
   t.deepEqual(List([1,2]), [1, 2], 'Looks like an array')
   t.equal(List([1, 2])[1], 2, 'can access like array')
   t.deepEqual(List([]), [], 'can create empty array')
+  t.deepEqual(List(), [], 'can create empty array via falsy')
   t.end()
 })
 
@@ -17,5 +20,29 @@ test('disallowed methods', t => {
 test('append', t => {
   t.deepEqual(List([]).append(1), [1], 'can append to empty list')
   t.deepEqual(List([0]).append(1), [0, 1], 'can append to list')
+  t.end()
+})
+
+test('prepend', t => {
+  t.deepEqual(List([]).prepend(1), [1], 'can prepend to empty list')
+  t.deepEqual(List([0]).prepend(1), [1, 0], 'can prepend to list')
+  t.end()
+})
+
+test('concat', t => {
+  t.deepEqual(List([]).concat([1]), [1], 'can concat to empty list')
+  t.deepEqual(List([0]).concat(List([1])), [0, 1], 'can concat to list')
+  t.deepEqual([0].concat(List([1])), [0, 1], 'can concat to native list')
+  t.end()
+})
+
+test('map', t => {
+  t.deepEqual(List([]).map(inc), [], 'can map empty list')
+  t.deepEqual(List([0, 1]).map(inc), [1, 2], 'can map list')
+  t.end()
+})
+
+test('pointed functor', t => {
+  t.deepEqual(List.of(1), [1])
   t.end()
 })
